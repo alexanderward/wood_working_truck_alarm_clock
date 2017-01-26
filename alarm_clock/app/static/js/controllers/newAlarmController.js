c84ad9fdc649b53bcc92a2409544f62a458b5c32
dagreenl <dgreenlees28@hotmail.com>
2017-01-25 23:20:49 -0600

    basic bindings set for new alarm and redesign.

--- alarm_clock/app/static/js/controllers/newAlarmController.js
+++ alarm_clock/app/static/js/controllers/newAlarmController.js
@@ -1,20 +1,39 @@
 
 app.controller('NewAlarmCtrl', function($scope, VideoService, $log){
+    // Configurations
     $('#action-Btn').html('<a class="btn btn-primary" href="#!/"><i class="fa fa-home"></i> Home</a>');
+    $scope.hstep = 1;
+    $scope.mstep = 1;
+    $scope.ismeridian = true;    
+
+    // Form
+    $scope.form = {
+        name: null,
+        alarmTime:  new Date(),
+        video: null,
+        flashingLights: true,
+        repeatDays: {
+            monday:false,
+            tuesday:false,
+            wednesday:false,
+            thursday:false,
+            friday:false,
+            saturday:false
+        }
+    };
+
     VideoService.getVideos()
         .then(function(data) {
-                    $scope.videos = data;
-                    console.log($scope.videos);
+                $scope.videos = data;
             }, function(error) {
                    console.log(error)
         });
     
-    $scope.mytime = new Date();
-    $scope.hstep = 1;
-    $scope.mstep = 1;
-    $scope.ismeridian = true;
+    
 
     $scope.changed = function () {
+        console.log($scope.form.alarmTime);
         $log.log('Time changed to: ' + $scope.mytime);
+        console.log($scope.form);
     };
 });
